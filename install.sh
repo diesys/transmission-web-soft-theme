9af676dc3fb2384fe74197a9cde4603656ddf2b8
sbiego <forsesim@anche.no>
2016-11-19 06:53:56 +0100

    added prompt for theme apply

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if [ ! -d "$(find /usr/share/transmission/ -iname "web" -type d)" ]
 	sleep 1
 
 else
-	installdir='/usr/share/transmission/'
+	installdir='/usr/share/transmission'
 	echo "Directory found! Installing in $installdir/web..."
 	sleep 1
 
@@ -29,4 +29,23 @@ fi
 
 cp -rv ./web/* ${installdir}/web/
 
-echo "Transmission theme successfully installed!"
+echo -e "\nTransmission soft theme successfully installed!\n \nWould you like to apply a soft theme now?\nNOTE:\w if you've already installed it, just modify the " $installdir"/web/index.html following the instructions on the wiki!"
+PS3=': '
+options=("Dark" "Light" "Quit")
+select opt in "${options[@]}"
+do
+	case $opt in
+    "Dark")
+				sed -i '21i\\t\t<link href="./style/transmission/soft-theme.min.css" type="text/css" rel="stylesheet" />\n\t\t<link href="style/transmission/soft-dark-theme.min.css" type="text/css" rel="stylesheet" />\n' ${installdir}/web/index.html;
+        echo "Soft dark theme successfully applied!";
+				break;;
+    "Light")
+        sed -i '21i\\t\t<link href="./style/transmission/soft-theme.min.css" type="text/css" rel="stylesheet" />\n\t\t<link href="style/transmission/soft-light-theme.min.css" type="text/css" rel="stylesheet" />\n' ${installdir}/web/index.html;
+        echo "Soft light theme successfully applied!";
+				break;;
+     "Quit")
+		 		echo 'You can always install it following the https://git.eigenlab.org/sbiego/transmission-web-soft-theme/wikis/home';
+        break;;
+      *) echo 'invalid option'
+			esac
+done
